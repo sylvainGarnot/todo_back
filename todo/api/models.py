8c63fb080d97f3780cf1bb01200b9b8f046e65be
sylvain <lilas.sylvain@gmail.com>
2020-06-23 12:07:00 +0200

    task model

--- todo/api/models.py
+++ todo/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Task(models.Model):
+    description = models.CharField(max_length=100, default='')
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.description
+
 # Create your models here.
