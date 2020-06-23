bef4a81cb92acbb23685672bf050a04f93287c59
sylvain <lilas.sylvain@gmail.com>
2020-06-23 12:20:45 +0200

    Set up Django REST Framework

--- todo/api/views.py
+++ todo/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+from .models import Task
+
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('description')
+    serializer_class = TaskSerializer
